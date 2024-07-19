6aa1f16b989c623aa9fdc05dbc3e358abb09a5ec
2004apurva <apurva162003@gmail.com>
2024-07-19 21:33:28 +0530

    basic webcam access code

--- Volume_By_Hand/Modue.py
+++ Volume_By_Hand/Modue.py
@@ -1,1 +1,28 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+
+
+Video = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+
+while True:
+    success , img = Video.read()
+
+    cv2.imshow("WebCam", img)
+
+    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF ==ord("q"):
+        
+        break
+
+
+Video.release()
+cv2.destroyAllWindows()
+
 
