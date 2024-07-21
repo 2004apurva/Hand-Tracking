24e4cca056ccfcb195576fdf25257f29704dbc60
2004apurva <apurva162003@gmail.com>
2024-07-21 13:24:29 +0530

    locating particular lm id

--- Volume_By_Hand/Modue.py
+++ Volume_By_Hand/Modue.py
@@ -6,9 +6,11 @@ import time
 
 
 # Saving the Webcam Video in an variable
+
 Video = cv2.VideoCapture(0)
 
 # Using the Mediapipe framework
+
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
@@ -19,22 +21,44 @@ past_Time = 0
 current_Time = 0
 
 # Code to capture/Run the Webcam Continues unless break key
+
 while True:
     success , img = Video.read()
 
     # Converting the original video to RGB
+
     imgRGB = cv2.cvtColor(img , cv2.COLOR_RGB2BGR)
     results = hands.process(imgRGB) # This will process the frame for us and give the result
 
 
     # Detects if there is an the hand in the webcam or not
     # Multi_hand  can also can be detected
+
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img,handLms, mpHands.HAND_CONNECTIONS) # mpHands.HAND_CONNECTIONS Basiclly connect all the hand points
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)  # mpHands.HAND_CONNECTIONS Basically connect all the hand points
+
+
+            # This line show the coordinates of each landmark in x,y,z
+
+            for id , lm in enumerate(handLms.landmark):
+
+                h, w, c = img.shape # Height , width , channel of the inage
+
+                # For finding the position of the center cx,cy are the center position
+                cx, cy = int(lm.x*w),int(lm.y*h) # x and y here are the coordinates of landmark
+                print(id,cx,cy)
+
+                # Detecting the id:0 of landmark and circling it
+                # Basically  tracking particular lm id in hand 
+                if id ==0:
+                    cv2.circle(img,(cx,cy),25,(255,0,255),cv2.FILLED)
+
+
 
 
     # Defining the FPS
+
     current_Time = time.time()
     fps = 1/(current_Time-past_Time)
     past_Time = current_Time
@@ -47,14 +71,17 @@ while True:
 
 
     # showing the webcam video
+
     cv2.imshow("WebCam", img)
 
     # important line to break the webcam terminal after pressing any key
+
     if cv2.waitKey(1) & 0xFF ==ord("q"):
         
         break
 
 # For closing the terminal after use
+
 Video.release()
 cv2.destroyAllWindows()
 
